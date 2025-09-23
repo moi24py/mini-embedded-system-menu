bc72603129964be57f0543a07e4aaa0c431e5ad7
moi24py <moi24py@gmail.com>
2025-09-23 16:20:15 +0200

    add print_menu() to main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_menu(){
+    printf("Read numbers\n");
+    printf("Read sentences\n");
+    printf("Exit\n");
+}
+
+int main(){
+
+    print_menu();
+    return 0;
+}
+
