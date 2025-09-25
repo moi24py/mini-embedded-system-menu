de531af24c54a4b6aac37e76ef542fe826b3137d
moi24py <moi24py@gmail.com>
2025-09-25 18:20:22 +0200

    add read() for numbers

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h> // for POSIX API, read()
+#include <ctype.h>
 
 // Prints menu
 void print_menu(){
@@ -37,42 +38,61 @@ int choice(){
 
 int main(){
 
-    // Numbers buffer
-    int numbers[200];
-    // Sentence buffer
-    char sentence[200];
+    char buffer[200];
+    // for read() returning value
+    ssize_t read_bytes;
 
     print_menu();
 
     int chosen_opt = choice();
 
-    // Check if the option is valid
+    // Checks if the option is valid
     if ( (chosen_opt < 1) || (chosen_opt > 3)){
         perror("The selected option is invalid. Only is 1, 2 or 3 are legit");
         return 1;
     }
 
+    
     switch (chosen_opt) {
-        // TODO
+        // Reads and prints numbers from stdin
         case 1:
             printf("Insert number: \n");
-            // if (read() == -1){
-            //    perror("Cannot read from stdin");
-            //    break;
-            //}
-       
+            fflush(stdout);
+            read_bytes = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+            if (read_bytes < 0){
+                perror("Cannot read from standard input");
+                return -1;
+            }
+            buffer[read_bytes] = '\0'; // Adds string terminator
+            // Process the input to extract numbers
+            printf("You entered: ");
+            for (int i = 0; i < read_bytes; i++) {
+                // If that char is a digit
+                if (isdigit(buffer[i])) {
+                    // Print only one digit in stdout
+                    putchar(buffer[i]);
+                    if (!isdigit(buffer[i+1])){
+                        putchar(32);
+                    }
+                }
+            }
+            printf("\n");
+            return 0;
+        
+        // Reads and prints chars from stdin
         case 2:
             printf("Insert a sentence: \n");
             fflush(stdout);
-            if (fgets(sentence, 200, stdin) != NULL){
-                printf("Your sentence:\n%s\n", sentence);
+            if (fgets(buffer, 200, stdin) != NULL){
+                printf("Your sentence:\n%s\n", buffer);
                 break;
             }
             else{
                 perror("Cannot read from standard input");
                 return 1;
             };
-
+        
+        // Exits the program
         case 3:
             printf("Bye bye\n");
             return 0;
